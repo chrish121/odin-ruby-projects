e04d81f55c4f5d4f5b3c641e2a0280ea10cf1ecd
Chris <123729087+chrish121@users.noreply.github.com>
2023-05-21 22:49:16 +0800

    Add code for sorting an array using the bubble sort methodology

---
+++ odin-bubble-sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(numbers)
+  numbers_length = numbers.length
+  n = 0
+  while n < numbers_length - 1 do
+    i = 0
+    while i < numbers_length - 1 do
+      if numbers[i] > numbers[i + 1]
+        first_number = numbers[i]
+        second_number = numbers[i + 1]
+        numbers[i] = "#{second_number}".to_i
+        numbers[i + 1] = "#{first_number}".to_i
+      end
+      i += 1
+    end
+    n += 1 
+  end
+  p numbers
+end
+
+bubble_sort([4,3,78,2,0,2])
