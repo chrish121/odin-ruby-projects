1bf18bc9aea68e1657d0c720927a23b2cc11a2a2
Chris <123729087+chrish121@users.noreply.github.com>
2023-05-21 23:05:50 +0800

    Add done_sorting variable and if else condition to check if the array is already sorted

--- odin-bubble-sort.rb
+++ odin-bubble-sort.rb
@@ -3,12 +3,19 @@ def bubble_sort(numbers)
   n = 0
   while n < numbers_length - 1 do
     i = 0
+    done_sorting = 0
     while i < numbers_length - 1 do
       if numbers[i] > numbers[i + 1]
         first_number = numbers[i]
         second_number = numbers[i + 1]
         numbers[i] = "#{second_number}".to_i
         numbers[i + 1] = "#{first_number}".to_i
+      else
+        done_sorting += 1
+        if done_sorting == numbers_length - 1
+          p numbers
+          return numbers
+        end
       end
       i += 1
     end
